7a78bc47abd19cf2f0f234bc674a7a7890339a03
LőrinczAlexandra <lorincz.alexandra@edu.bme.hu>
2021-12-16 09:50:12 +0100

    scope added

--- src/main/java/TestRunner/Runner.java
+++ src/main/java/TestRunner/Runner.java
@@ -2,7 +2,7 @@ package TestRunner;
 
 import org.junit.runner.RunWith;		
 import cucumber.api.CucumberOptions;		
-import cucumber.api.junit.Cucumber;		
+import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)				
 @CucumberOptions(monochrome = true,
